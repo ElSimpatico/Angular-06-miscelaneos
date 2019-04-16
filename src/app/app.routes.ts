2b04531f6bf0578bccfa950629aafe1d50912c3c
Aaron Velasco <tiquismiquisss@gmail.com>
2019-04-16 15:26:40 +0200

    modularizar rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,18 +5,12 @@ import { UsuarioComponent } from './components/usuario/usuario.component';
 import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
 import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
 import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
 
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario',
-      component: UsuarioComponent,
-      children: [
-        { path: 'nuevo', component: UsuarioNuevoComponent },
-        { path: 'editar', component: UsuarioEditarComponent },
-        { path: 'detalle', component: UsuarioDetalleComponent },
-        { path: '**', pathMatch: 'full', redirectTo: 'nuevo' 
-    }] },
+    { path: 'usuario', component: UsuarioComponent, children: USUARIO_ROUTES},
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
