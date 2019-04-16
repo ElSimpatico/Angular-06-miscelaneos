33b72a1a71035246b4f55f072251517488d8a46c
Aaron Velasco <tiquismiquisss@gmail.com>
2019-04-16 09:00:04 +0200

    aplicar estilos propios al componente

--- src/app/components/ng-style/ng-style.component.ts
+++ src/app/components/ng-style/ng-style.component.ts
@@ -3,17 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   template: `
+  <button class="btn btn-primary" (click)="tamano = tamano + 5">
+    <i class="fa fa-plus"></i>
+  </button>
+
+  <button class="btn btn-primary" (click)="tamano = tamano - 5">
+    <i class="fa fa-minus"></i>
+  </button>
     <p [style.fontSize.px]="tamano">
       Hola mundo... esta es una etiqueta
     </p>
 
-    <button class="btn btn-primary" (click)="tamano = tamano + 5">
-      <i class="fa fa-plus"></i>
-    </button>
-
-    <button class="btn btn-primary" (click)="tamano = tamano - 5">
-      <i class="fa fa-minus"></i>
-    </button>
   `,
   styles: []
 })
